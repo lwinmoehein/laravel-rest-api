5183a9cf13a1714c4371511a697159f3cfe93c89
lwinmoehein <dev.lwinmoehein@gmail.com>
2019-07-21 21:50:52 +0630

    index and show/id

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Http\Resources\Json\Resource;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Resource::withoutWrapping();
+        Schema::defaultStringLength(191);
     }
 }
