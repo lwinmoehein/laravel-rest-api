5f0abea5fd463e5f83a563b8ec45daac9e561399
lwinmoehein <dev.lwinmoehein@gmail.com>
2019-07-21 22:41:41 +0630

    added create and update

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -30,7 +30,14 @@ class ArticleController extends Controller
     public function store(Request $request)
     {
         //
-        
+        $article=$request->isMethod('put')?Article::findOrFail($request->article_id):new Article;
+        $article->id=$request->input('article_id');
+        $article->title=$request->input('title');
+        $article->body=$request->input('body');
+
+        if($article->save()){
+            return new ArticleResource($article);
+        }
     }
 
     /**
